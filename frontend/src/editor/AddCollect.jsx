e5d55dfc9832bd44a9c928ddfaf52d703837a44b
vincent youmssi <vincentyoumssi@gmail.com>
2024-05-31 01:26:14 +0100

    add modif to project #4

--- frontend/src/editor/AddCollect.jsx
+++ frontend/src/editor/AddCollect.jsx
@@ -1,24 +1,24 @@
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { postCollectionRequest } from '../services/requests'
-import { useMutation } from '@tanstack/react-query'
-import { useDispatch, useSelector } from 'react-redux'
-import { setNotification } from '../stores/NotificationReducer'
-import { appendUserCollection } from '../stores/userCollections'
-import { setToken } from '../services/requests'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { postCollectionRequest } from "../services/requests";
+import { useMutation } from "@tanstack/react-query";
+import { useDispatch, useSelector } from "react-redux";
+import { setNotification } from "../stores/NotificationReducer";
+import { appendUserCollection } from "../stores/userCollections";
+import { setToken } from "../services/requests";
 
 export default function AddCollect() {
   const [collectionInfos, setCollectionInfos] = useState({
-    title: '',
-    tags: '',
-    description: '',
-    country: '',
-  })
-  const user = useSelector(({ user }) => user)
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  console.log(user)
-  user && setToken(user)
+    title: "",
+    tags: "",
+    description: "",
+    country: "",
+  });
+  const user = useSelector(({ user }) => user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  console.log(user);
+  user && setToken(user);
 
   // async function addCollection(data) {
   //   const res = await postCollectionRequest(data)
@@ -28,15 +28,15 @@ export default function AddCollect() {
   const addCollectionMutation = useMutation({
     mutationFn: postCollectionRequest,
     onSuccess: (res) => {
-      console.log(res)
-      navigate(`/my-collections/${res.user}`)
-      dispatch(appendUserCollection(res))
+      console.log(res);
+      navigate(`/my-collections/${res.user}`);
+      dispatch(appendUserCollection(res));
       dispatch(
         setNotification({
           msg: `${res.title} added successfully`,
-          clr: 'green',
+          clr: "green",
         })
-      )
+      );
     },
     onError: (error) =>
       dispatch(
@@ -44,71 +44,81 @@ export default function AddCollect() {
           msg: error.response.data.error
             ? error.response.data.error
             : error.message,
-          clr: 'red',
+          clr: "red",
         })
       ),
-  })
+  });
 
   function handleInputs(e) {
-    const { value, name } = e.target
-    setCollectionInfos({ ...collectionInfos, [name]: value })
+    const { value, name } = e.target;
+    setCollectionInfos({ ...collectionInfos, [name]: value });
   }
 
   async function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    addCollectionMutation.mutate(collectionInfos)
+    addCollectionMutation.mutate(collectionInfos);
   }
 
   return (
-    <section className='login-register-section container'>
-      <form className='form-container login' onSubmit={handleSubmit}>
-        <h1 className='heading'>Add collect</h1>
-        <div className='input_box'>
+    <section className="login-register-section container">
+      <form className="form-container login" onSubmit={handleSubmit}>
+        <h1 className="heading">Add collect</h1>
+        <div className="input_box">
           <input
-            id='email'
-            type='text'
-            placeholder='Enter title'
-            name='title'
-            className='box'
+            type="text"
+            placeholder="Enter title"
+            name="title"
+            className="box"
             onChange={handleInputs}
           />
         </div>
-        <div className='input_box'>
+        <div className="input_box">
           <input
-            id='profession'
-            type='text'
-            placeholder='Enter tags t#t#...'
-            name='tags'
-            className='box'
+            type="text"
+            placeholder="Enter tags t#t#..."
+            name="tags"
+            className="box"
+          />
+        </div>
+        <div className="input_box">
+          <input
+            type="text"
+            placeholder="Enter country"
+            name="country"
+            className="box"
             onChange={handleInputs}
           />
         </div>
-        <div className='input_box'>
+        <div className="input_box">
           <textarea
-            name='description'
-            id='description'
-            className='box'
-            placeholder='Enter description'
-            min='000000000'
-            max='999999999'
-            rows='3'
-            cols='10'
+            name="description"
+            className="box"
+            placeholder="Enter description"
+            min="000000000"
+            max="999999999"
+            rows="3"
+            cols="10"
             onChange={handleInputs}
           ></textarea>
         </div>
-        <div className='input_box'>
+        <div className="input_box">
           <input
-            id='ccountry'
-            type='text'
-            placeholder='Enter country'
-            name='country'
-            className='box'
+            type="text"
+            placeholder="Enter country"
+            name="country"
+            className="box"
             onChange={handleInputs}
           />
         </div>
-        <input type='submit' value='create' name='add-post' className='btn' />
+        <input type="submit" value="create" name="add-post" className="btn" />
+        <input
+          type="submit"
+          value="create"
+          name="add-collect"
+          className="btn"
+        />
       </form>
     </section>
-  )
+  );
 }
--- frontend/src/editor/UpdateCollect.jsx
+++ frontend/src/editor/UpdateCollect.jsx
@@ -1,103 +1,107 @@
-import { useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import { updateCollectionRequest } from '../services/requests'
-import { useMutation } from '@tanstack/react-query'
-import { useDispatch } from 'react-redux'
-import { setNotification } from '../stores/NotificationReducer'
-import { updateUserCollection } from '../stores/userCollections'
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { updateCollectionRequest } from "../services/requests";
+import { useMutation } from "@tanstack/react-query";
+import { useDispatch } from "react-redux";
+import { setNotification } from "../stores/NotificationReducer";
+import { updateUserCollection } from "../stores/userCollections";
 
 export default function UpdateCollect() {
   const [collectionInfos, setCollectionInfos] = useState({
-    title: '',
-    tags: '',
-    description: '',
-    country: '',
-  })
-  const id = useParams().id
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+    title: "",
+    tags: "",
+    description: "",
+    country: "",
+  });
+  const id = useParams().id;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const updateCollectionMutation = useMutation({
     mutationFn: updateCollectionRequest,
     onSuccess: (res) => {
-      console.log(res)
-      dispatch(updateUserCollection(res))
-      navigate(`/my-collections/${res.user}`)
+      console.log(res);
+      dispatch(updateUserCollection(res));
+      navigate(`/my-collections/${res.user}`);
     },
     onError: (error) =>
       dispatch(
         setNotification({
           msg: error.response.data.error,
-          clr: 'red',
+          clr: "red",
         })
       ),
-  })
+  });
 
   function handleInputs(e) {
-    const { value, name } = e.target
-    setCollectionInfos({ ...collectionInfos, [name]: value })
+    const { value, name } = e.target;
+    setCollectionInfos({ ...collectionInfos, [name]: value });
   }
 
   async function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
     // collectionInfos.id = id
-    updateCollectionMutation.mutate({ ...collectionInfos, id })
+    updateCollectionMutation.mutate({ ...collectionInfos, id });
   }
 
   return (
-    <section className='login-register-section container'>
-      <form className='form-container login' onSubmit={handleSubmit}>
-        <h1 className='heading'>Update collect</h1>
-        <div className='input_box'>
+    <section className="login-register-section container">
+      <form className="form-container login" onSubmit={handleSubmit}>
+        <h1 className="heading">Update collect</h1>
+        <div className="input_box">
           <input
-            id='colTitle'
-            type='text'
-            placeholder='Enter title'
-            name='title'
-            className='box'
+            type="text"
+            placeholder="Enter title"
+            name="title"
+            className="box"
             onChange={handleInputs}
           />
         </div>
-        <div className='input_box'>
+        <div className="input_box">
           <input
-            id='colprofession'
-            type='text'
-            placeholder='Enter tags t#t#...'
-            name='tags'
-            className='box'
+            type="text"
+            placeholder="Enter tags t#t#..."
+            name="tags"
+            className="box"
             onChange={handleInputs}
           />
         </div>
-
-        <div className='input_box'>
+        <div className="input_box">
+          <input
+            type="text"
+            placeholder="Enter country"
+            name="country"
+            className="box"
+            onChange={handleInputs}
+          />
+        </div>
+        <div className="input_box">
           <textarea
-            name='description'
-            id='coldescription'
-            className='box'
-            placeholder='Enter description'
-            min='000000000'
-            max='999999999'
-            rows='3'
-            cols='10'
+            name="description"
+            className="box"
+            placeholder="Enter description"
+            min="000000000"
+            max="999999999"
+            rows="3"
+            cols="10"
             onChange={handleInputs}
           ></textarea>
         </div>
-        <div className='input_box'>
+        <div className="input_box">
           <input
-            name='country'
-            id='colcountry'
-            className='box'
-            placeholder='Enter country'
+            name="country"
+            className="box"
+            placeholder="Enter country"
             onChange={handleInputs}
           />
         </div>
         <input
-          type='submit'
-          value='update collect'
-          name='update-collect'
-          className='btn'
+          type="submit"
+          value="update"
+          name="update-collect"
+          className="btn"
         />
       </form>
     </section>
-  )
+  );
 }
