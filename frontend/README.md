0937118e2b69cc2b126ebb6f37551a2d81af246b
vincent youmssi <vincentyoumssi@gmail.com>
2024-05-31 06:27:26 +0100

    final deployment

--- frontend/README.md
+++
@@ -1,1 +0,0 @@
-# SavEarth Project
--- frontend/src/components/Loader.jsx
+++
@@ -1,8 +0,0 @@
-export default function Loader() {
-  return (
-    <div class="loader">
-      <div class="loader-wheel"></div>
-      <div class="loader-text"></div>
-    </div>
-  );
-}
